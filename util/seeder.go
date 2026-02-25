79967294003f60aff81029e9a0cfcd33c1ade7db
yyyushiro <yushiro0418@gmail.com>
2026-02-25 06:45:46 -0500

    FEAT: Added utility functions for setting DB.

--- util/seeder.go
+++ util/seeder.go
@@ -10,13 +10,26 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-func main() {
-	// Opens database
-	db, err := sql.Open("sqlite", "datePlans.db")
+var db *sql.DB
+
+func openDB() {
+	var err error
+	db, err = sql.Open("sqlite", "/Users/yushiro/src/projects/date_proposal_app/datePlans.db")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func main() {
+	openDB()
 	defer db.Close()
+	//addRandomPlans()
+	//autoCategorize()
+	setLikeToZero()
+}
+
+func addRandomPlans() {
 
 	places := []string{"新宿", "渋谷", "横浜", "自宅", "公園", "水族館", "夜の海", "知らない駅"}
 	actions := []string{"散歩する", "アイスを食べる", "夜景を見る", "ゲームをする", "深海魚を眺める", "お弁当を食べる"}
@@ -48,5 +61,51 @@ func main() {
 	tx.Commit()
 
 	fmt.Printf("Transaction completed; total time: %d \n", time.Since(start))
+}
 
+func autoCategorize() {
+	categories := []string{"Romantic", "Adventurous", "Budget-Friendly", "Foodie", "Indoor"}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tx.Rollback()
+	// 1. Get all IDs from the database
+	var rows *sql.Rows
+	rows, err = tx.Query(`SELECT id FROM datePlans`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// 2. Prepare the update statement
+	var stmt *sql.Stmt
+	stmt, err = tx.Prepare("UPDATE datePlans SET category = ? WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	for rows.Next() {
+		var id int
+		rows.Scan(&id)
+
+		// Pick a random category from our list
+		randomCat := categories[rand.Intn(len(categories))]
+
+		// Apply it to this specific ID
+		_, err = stmt.Exec(randomCat, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	tx.Commit()
+}
+
+func setLikeToZero() {
+	query := `UPDATE datePlans SET "like" = 0 WHERE "like" IS NULL;`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
